e917923b065158e5f2505319bc17d3dab4255e81
Unknown <hammarstrand@hotmail.com>
2019-03-18 08:49:50 +0100

    Reinitialize with authentication

---
+++ CrossLogger/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CrossLogger.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
